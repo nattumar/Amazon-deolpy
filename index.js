34ea0ba143a40273e1369f35e32029b008562b97
nattumar <nattumar3@gmail.com>
2024-08-16 15:10:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
-    app.use(cors({ origin: true})
+    app.use(cors()
 );
 app.use(express.json());
 app.get("/",(req, res) => {
